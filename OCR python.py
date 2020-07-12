901930cb95fe14372bd8fc5764116780b74a3d21
kaustubh43 <kaustubhajgaonkar43@gmail.com>
2020-07-12 13:52:25 +0530

    image to .txt working

--- OCR python.py
+++ OCR python.py
@@ -1,8 +1,10 @@
 #importing
-import PIL
+from PIL import Image
 import sys
 from pdf2image import convert_from_path
 import os
+import pytesseract
+
 print('Start with OCR project')
 
 'Converting to image from PDF'
@@ -17,7 +19,34 @@ for image in images:
     filename="image"+str(imagecounter)+".jpg"
     image.save(filename,'JPEG')
     imagecounter+=1
+
     
+'Converting Image to text file'
+
+files=imagecounter-1
+
+output_file='output_file.txt'
+
+f=open(output_file,'a')
+
+#sepcifying tesseract installation location
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+for i in range(1,files+1):
+
+    filename="image"+str(i)+".jpg"
+
+
+    text=str(((pytesseract.image_to_string(Image.open(filename),lang='eng'))))
+              
+    #text=text.replace('-\n','')
+
+    f.write(text)
+
+f.close()
+    
+
+
     
 
 
